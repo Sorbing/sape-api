54419c298877bbbeba33dcdf15690f883ece4f05
Stanislav Butsenko <svbutsenko@gmail.com>
2012-06-15 00:18:40 +0300

    Refactoring a Base Class

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
-xml-rpc-Sape.ru-api
+SapeApi Class
 ===================
 
-small xml-rpc api wrapper for sape.ru
+Description
+-----------
+Simple XML-RPC API Client for SAPE.ru
+
+Require
+-------
+XML-RPC for PHP: http://sourceforge.net/projects/phpxmlrpc/files/phpxmlrpc/
+
+Example
+-------
+Initialize:
+	require 'xmlrpc.inc'; // required this library
+	require 'SapeApi.class.php';
+	
+	$user = 'your_login_on_sape.ru';
+	$pass = 'your_password_on_sape.ru';
+
+	$sape = new SapeApi();
+
+Authorization:
+	$sape->set_debug(0)->connect($user, $pass);
+
+Get xmlrpcresp response:
+	/* @var $response xmlrpcresp */
+	$response = $connect->query('sape.get_site_pages', 88888, 111)->exec();
+	if ($response->faultCode()) {
+	  echo '[' . $response->faultCode() . '] ' . $response->faultString();
+	  exit;
+	}
+	/* @var $pages array */
+	$pages = php_xmlrpc_decode($response->value());
+
+Get all projects:
+  $projects = $sape->query('sape.get_projects')->fetch();
+
+Get 12 projects without removed:
+	/* @var $projects array */
+	$projects = $sape->query('sape.get_projects', 0, 1, 12)->fetch();
+
+Get project by id:
+	/* @var $project array */
+	$project = $sape->query('sape.get_project', 123456)->fetch();
+
+Error handling:
+	$projects = $sape->query('sape.get_projects')->fetch();
+	if ($sape->get_errnum()) {
+		echo '[' . $sape->get_errnum() . '] ' . $sape->get_error();
+	}
--- SapeApi.class.php
+++ SapeApi.class.php
@@ -1,188 +1,240 @@
 <?
-class SapeApiException extends Exception{}
+class SapeApiException extends Exception {}
 
 /**
-* SapeApi
-* 
-* интерфейс для работы с xml-rpc sape.ru
-* потребуется библиотека xml-rpc для php http://sourceforge.net/projects/phpxmlrpc/files/phpxmlrpc/3.0.0beta/xmlrpc-3.0.0.beta.zip/download
-* 
-* пример:
-* require_once './3rdparty/xmlrpc-3.0.0.beta/lib/xmlrpc.inc';
-* require_once 'sape_api.class.php';
-* $sape_xml = new SapeApi;
-* $connect = $sape_xml->set_debug(0)->connect();
-* $get_user = $connect->query('sape.get_user')->cmd(); // метод без аргументов
-* $get_site_pages = $connect->query('sape.get_site_pages', array(88888))->cmd(); // метод с одним аргументом
-* $get_site_pages = $connect->query('sape.get_site_pages', array(88888, 111))->cmd(); // метод с двумя аргументами
-*
-* @author Frenk1 aka Gudd.ini
-* @version 0
-*/
-class SapeApi{
-    /**
-    * Свойства с данными для соединения с сервером xml-rpc
-    */
-    protected $path = '/xmlrpc/';
-    protected $host = 'api.sape.ru';
-    protected $port = 80;
+ * SapeApi
+ *
+ * интерфейс для работы с xml-rpc sape.ru
+ * потребуется библиотека xml-rpc для php http://sourceforge.net/projects/phpxmlrpc/files/phpxmlrpc/3.0.0beta/xmlrpc-3.0.0.beta.zip/download
+ *
+ * пример:
+ * require_once './3rdparty/xmlrpc-3.0.0.beta/lib/xmlrpc.inc';
+ * require_once 'sape_api.class.php';
+ * $sape_xml = new SapeApi;
+ * $connect = $sape_xml->set_debug(0)->connect();
+ * $get_user = $connect->query('sape.get_user')->exec(); // метод без аргументов
+ * $get_site_pages = $connect->query('sape.get_site_pages', 88888, 111)->exec();
+ *
+ * @author Frenk1 aka Gudd.ini,
+ * @version 0.1
+ * @link http://phpxmlrpc.sourceforge.net/doc-2/
+ */
+class SapeApi
+{
+	/**
+	 * Свойства с данными для соединения с сервером xml-rpc
+	 */
+	protected $_path = '/xmlrpc/?v=extended';
+	protected $_host = 'api.sape.ru';
+	protected $_port = 80;
 
-    /**
-    * Свойства с данными для авторизации
-    */
-    protected $login = 'login';
-    protected $password = 'md5_hash'; // md5
+	/**
+	 * Уровень режима отладки (0, 1, 2)
+	 */
+	private $_debug = 0;
 
-    /**
-    * Уровень режима отладки
-    * 0, 1, 2
-    */
-    private $debug = 0;
+	/**
+	 * Объект текущего соединения
+	 * @link http://phpxmlrpc.sourceforge.net/doc-2/
+	 * @var	xmlrpc_client
+	 */
+	private $_xmlrpc = false;
 
-    /**
-    * объект текущего соединения
-    */
-    private $connect = false;
+	/**
+	 * текущие куки
+	 */
+	private $_cookies = false;
 
-    /**
-    * текущие куки
-    */
-    private $cookies = false;
+	/**
+	 * Результат выполнения последнего запроса
+	 * @link http://phpxmlrpc.sourceforge.net/doc-1.1/ch05s03.html
+	 * @var xmlrpcresp
+	 */
+	private $_response = false;
 
-    /**
-    * результат последнего запроса
-    */
-    private $response = false;
+	/**
+	 * Последний сохраненный запрос
+	 */
+	private $_query = false;
 
-    /**
-    * последний сохраненный запрос
-    */
-    private $query = false;
+	/**
+	 * Ошибка выполнения запроса
+	 * @var string
+	 */
+	private $_error = '';
 
+	/**
+	 * Номер ошибки
+	 * @var integer
+	 */
+	private $_errnum = 0;
 
-    /**
-    * метод для подключения к серверу и поддержания соединения
-    */
-    function connect() {
-        $this->connect = new xmlrpc_client(
-            $this->path,
-            $this->host,
-            $this->port
-            );
-        $this->connect->setDebug($this->debug);
+	/**
+	 * Подключение к серверу и поддержка соединения
+	 * @param	string	$user	username SAPE
+	 * @param	string	$pass	password SAPE
+	 * @throws SapeApiException
+	 * @return SapeApi
+	 * @link http://phpxmlrpc.sourceforge.net/doc-2/
+	 */
+	function connect($user, $pass)
+	{
+		$this->_xmlrpc = new xmlrpc_client(
+			$this->_path, $this->_host, $this->_port
+		);
+		$this->_xmlrpc->setDebug($this->_debug);
 
-        $query = new xmlrpcmsg(
-            'sape.login',
-            array(
-                    php_xmlrpc_encode($this->login),
-                    php_xmlrpc_encode($this->password),
-                    php_xmlrpc_encode(true)
-                )
-            );
-        $this->response = $this->connect->send($query);
+		$query = new xmlrpcmsg(
+				'sape.login',
+				array(
+					php_xmlrpc_encode($user),
+					php_xmlrpc_encode(md5($pass)),
+					php_xmlrpc_encode(true)
+				)
+		);
+		$this->_response = $this->_xmlrpc->send($query);
 
-        try {
-            if ( !$this->response->value()->scalarval() ) {
-                throw new SapeApiException('Не пришел user_id от сервера');
-            }
+		try {
+			if ( !$this->_response->value()->scalarval() ) {
+				throw new SapeApiException('Не пришел user_id от сервера');
+			}
 
-        } catch (SapeApiException $e) {
-            echo $e->getMessage();
-        }
+		} catch (SapeApiException $e) {
+			echo $e->getMessage();
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-    * установка уровня дебага
-    */
-    function set_debug($lvl = NULL) {
-        if (!is_null($lvl)) {
-            $lvl = intval($lvl);
-            $this->debug = $lvl;
-        }
+	/**
+	 * Установить уровень отладки
+	 * @return SapeApi
+	 */
+	public function set_debug($lvl = NULL)
+	{
+		if (!is_null($lvl)) {
+			$lvl = intval($lvl);
+			$this->_debug = $lvl;
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-    * получение свежих кук от сервера
-    */
-    function get_cookies() {
-        $this->cookies = $this->response->_cookies;
-    }
+	/**
+	 * Cгенерировать запрос
+	 * @param	string	$sape_method	метод запроса
+	 * @param	mix		$arg1	аргумент
+	 * @param	mix		$arg2	аргумент
+	 * @param	mix		$argN	аргумент N
+	 * @return SapeApi
+	 * @link http://phpxmlrpc.sourceforge.net/doc-2/
+	 */
+	public function query($sape_method, $arg1 = null, $arg2 = null, $argN = null)
+	{
+		$args = array_slice(func_get_args(), 1);
 
-    /**
-    * синхронизация кук при каждом запросе
-    */
-    function sync_cookies() {
-        $this->get_cookies();
+		if (count($args)) {
+			foreach ($args as $arg) {
+				$sape_args[] = php_xmlrpc_encode($arg);
+			}
+			$this->_query = new xmlrpcmsg($sape_method, $sape_args);
+		} else {
+			$this->_query = new xmlrpcmsg($sape_method);
+		}
 
-        foreach ($this->response->_cookies as $name => $value) {
-            $this->connect->setCookie($name, $value['value']);
-        }
-    }
+		return $this;
+	}
 
-    /**
-    * генерирование запроса к серверу sape
-    */
-    function query() {
-        $num_args = func_num_args();
-        $args = array();
-        $sape_method = func_get_arg(0);
+	/**
+	 * Выполнить запрос
+	 * @link http://phpxmlrpc.sourceforge.net/doc-1.1/ch05s03.html
+	 * @return xmlrpcresp	Methods: faultCode(), faultString(), value(), etc..
+	 */
+	public function exec()
+	{
+		try {
+			if ( ! $this->_query) {
+				throw new SapeApiException('Нет запроса для выполнения');
+			}
+		} catch (SapeApiException $e) {
+			echo $e->getMessage();
+		}
 
-        if ($num_args > 1) {
-            foreach (func_get_args() as $num => $arg) {
-                if ($num < 1) continue;
-                if ($num == 1) {
-                    foreach ($arg as $a) {
-                        $args[] = php_xmlrpc_encode($a);
-                    }
-                }
-            }
-            $this->query = new xmlrpcmsg($sape_method, $args);
+		$this->_sync_cookies();
 
-        } else {
-            $this->query = new xmlrpcmsg($sape_method);
-        }
+		$this->_response = $this->_xmlrpc->send($this->_query);
 
-        return $this;
-    }
+		try {
+			if ($this->_response->faultCode()) {
+				throw new SapeApiException('Сервер sape сообщил об ошибке');
+			}
 
-    /**
-    * выполнение запроса к серверу
-    */
-    function cmd() {
-        try {
-            if (!$this->query) {
-                throw new SapeApiException('Нет запроса для выполнения');
-            }
+		} catch (SapeApiException $e) {
+			echo $e->getMessage();
+		}
 
-        } catch (SapeApiException $e) {
-            echo $e->getMessage();
-        }
+		return $this->_response;
+	}
 
-        $this->sync_cookies();
-        $this->response = $this->connect->send($this->query);
+	/**
+	 * Выполнить запрос, извлечь данные, обработать ошибки
+	 * @return array
+	 */
+	public function fetch()
+	{
+		$response = $this->exec();
+		if ($response->faultCode()) {
+			$this->_set_errnum($response->faultCode());
+			$this->_set_error($response->faultString());
+			return false;
+		}
 
-        try {
-            if ($this->response->faultCode()) {
-                throw new SapeApiException('Сервер sape сообщил об ошибке');
-            }
+		return php_xmlrpc_decode($response->value());
+	}
 
-        } catch (SapeApiException $e) {
-            echo $e->getMessage();
-        }
+	/**
+	 * получение свежих кук от сервера
+	 */
+	function get_cookies() {
+		$this->_cookies = $this->_response->_cookies;
+	}
 
-        return $this->response;
-    }
+	/**
+	 * Синхронизировать куки (при каждом запросе)
+	 * @return void
+	 */
+	private function _sync_cookies()
+	{
+		$this->get_cookies();
+		foreach ($this->_response->_cookies as $name => $value) {
+			$this->_xmlrpc->setCookie($name, $value['value']);
+		}
+	}
 
-    /**
-    * получение данных, которые остались после последнего выполненного запроса
-    */
-    function get_response() {
-        return $this->response;
-    }
+	/**
+	 * Получить результат выполненного запроса
+	 * @return xmlrpcresp
+	 */
+	public function get_response()
+	{
+		return $this->_response;
+	}
+
+	private function _set_error($error) {
+		$this->_error = $error;
+	}
+
+	public function get_error() {
+		return $this->_error;
+	}
+
+	public function get_errnum() {
+		return $this->_errnum;
+	}
+
+	private function _set_errnum($errnum) {
+		$this->_errnum = $errnum;
+	}
 
 }
 
